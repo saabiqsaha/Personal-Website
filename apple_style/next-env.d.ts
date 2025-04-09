d174a8eca233a6120c8fe2a22d73c3e61ff03794
Mohammed Saabiq Saha <138797077+saabiqsaha@users.noreply.github.com>
2025-04-09 20:51:02 +0000

    bad work

--- apple_style/next-env.d.ts
+++ apple_style/next-env.d.ts
@@ -1,11 +1,5 @@
 /// <reference types="next" />
-/// <reference types="react" />
-/// <reference types="react-dom" />
+/// <reference types="next/image-types/global" />
 
-declare namespace React {
-  interface JSX {
-    // Allow any element
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    [elem: string]: any;
-  }
-} 
+// NOTE: This file should not be edited
+// see https://nextjs.org/docs/app/building-your-application/configuring/typescript for more information.
